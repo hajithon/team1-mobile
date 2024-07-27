8ccd596708772603dd93a3c2350dd08daaa31586
nx006 <limazero14@gmail.com>
2024-07-27 19:03:42 +0900

    view: 뉴스 카드 컴포넌트

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hajithon_teami_flutter_app/component/home/news_card.dart';
 import 'package:hajithon_teami_flutter_app/pages/common/default_layout.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,6 +9,14 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultLayout(child: Center(child: Text('Home')));
+    return const DefaultLayout(
+        child: Center(
+            child: NewsCard(
+      title: '좋은 아침이에요!',
+      headlines: [
+        '오픈AI, 검색 엔진 공개.. 구글에 도전장',
+        '테슬라, 1조 달러 시가총액 돌파',
+      ],
+    )));
   }
 }
