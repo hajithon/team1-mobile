dbdddd9aad2763891fcbf4154841bc08bc50b422
nx006 <limazero14@gmail.com>
2024-07-27 20:59:43 +0900

    feat: 뉴스, 위클리 카드 추가

---
+++ lib/component/home/weekly_strict_card.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:hajithon_teami_flutter_app/component/common/custom_text_style.dart';
+import 'package:hajithon_teami_flutter_app/const/color/color.dart';
+import 'package:hajithon_teami_flutter_app/view_model/strict/strict_model.dart';
+import 'package:intl/intl.dart';
+
+class WeeklyStrictCard extends StatelessWidget {
+  final String title;
+  final List<StrictModel> stricts;
+
+  const WeeklyStrictCard({
+    super.key,
+    required this.title,
+    required this.stricts,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: inputBackgroundColor),
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: TextStyles.titleTextStyle.copyWith(
+                  fontSize: 16.0,
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  '전체 보기',
+                  style: TextStyles.subTitleTextStyle,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 12.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              for (int i = 0; i < stricts.length; i++) ...[
+                if (i > 0) const SizedBox(width: 8.0),
+                Expanded(
+                  child: _StrictDayBox(
+                    date: stricts[i].date,
+                    wakeUpState: stricts[i].wakeUpState,
+                  ),
+                ),
+              ],
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _StrictDayBox extends StatelessWidget {
+  final DateTime date;
+  final WakeUpState? wakeUpState;
+
+  const _StrictDayBox({required this.date, this.wakeUpState});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.0,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4.0),
+          color: _getBackgroundColor(),
+        ),
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Text(
+            DateFormat('E', 'ko_KR').format(date),
+            style: TextStyles.subTitleTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Color _getBackgroundColor() {
+    switch (wakeUpState) {
+      case WakeUpState.wakeUp:
+        return Colors.green;
+      case WakeUpState.wakeUpLate:
+        return Colors.red;
+      case WakeUpState.quizCompleted:
+        return Colors.blue;
+      default:
+        return Colors.grey;
+    }
+  }
+}
