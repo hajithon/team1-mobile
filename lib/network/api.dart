d085659e4b6657de2a0ff9f68b9636d1fd294236
Inhwa Jang <superjang3357@naver.com>
2024-07-28 04:56:08 +0900

    add log middleware to dio

--- lib/network/api.dart
+++ lib/network/api.dart
@@ -1,5 +1,6 @@
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' hide LogInterceptor;
 import 'package:hajithon_teami_flutter_app/network/interceptors/jwt.dart';
+import 'package:hajithon_teami_flutter_app/network/interceptors/log.dart';
 
 class ApiProvider {
   final baseUrl = 'https://2bzksr2k-8000.asse.devtunnels.ms/';
@@ -7,5 +8,6 @@ class ApiProvider {
   ApiProvider() {
     dio.options.baseUrl = baseUrl;
     dio.interceptors.add(JWTInterceptor(dio));
+    dio.interceptors.add(LogInterceptor());
   }
 }
