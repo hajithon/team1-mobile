d45bf28c7a848ad87f1a25dccfdd3e022864431a
Inhwa Jang <superjang3357@naver.com>
2024-07-28 05:37:33 +0900

    feat : stric

--- lib/pages/home/binding.dart
+++ lib/pages/home/binding.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:hajithon_teami_flutter_app/services/group/service.dart';
 import 'package:hajithon_teami_flutter_app/services/news/service.dart';
+import 'package:hajithon_teami_flutter_app/services/profile/service.dart';
 import 'package:hajithon_teami_flutter_app/services/todo/service.dart';
 
 class HomePageBinding implements Bindings {
@@ -9,5 +10,6 @@ class HomePageBinding implements Bindings {
     Get.lazyPut(() => NewsService());
     Get.lazyPut(() => TodoService());
     Get.lazyPut(() => GroupService());
+    Get.lazyPut(() => ProfileService());
   }
 }
--- lib/services/profile/service.dart
+++ lib/services/profile/service.dart
@@ -8,10 +8,34 @@ class ProfileService extends GetxController {
 
   final Rx<Profile?> _profile = Rx(null);
   Profile? get profile => _profile.value;
+
+  final Rx<List<Wake>> _wakes = Rx([]);
+  List<Wake> get wakes => _wakes.value;
+
   ProfileService({ProfileRepository? repository}) : repository = repository ?? ProfileRepository();
 
   Future<void> fetchProfile() async {
     _profile.value = await repository.getProfile();
+  }
+
+  Future<void> fetchWakes() async {
+    _wakes.value = await repository.getWakesByMonth(DateTime.now().month);
+  }
+
+  Wake? getWakeByDay(int year, int month, int day) {
+    return wakes.firstWhereOrNull(
+      (wake) => wake.wakeTime.year == year && wake.wakeTime.month == month && wake.wakeTime.day == day,
+    );
+  }
+
+  List<Wake?> getWeaklyWakes() {
+    DateTime now = DateTime.now();
+    int weekday = now.weekday % 7;
+    List<Wake?> res = [];
+    for (int i = 0; i < 7; i++) {
+      res.add(getWakeByDay(now.year, now.month, now.day - weekday + i));
+    }
+    return res;
   }
 
   Future<void> patchProfile({XFile? profileImage, String? nickname, String? description}) async {
