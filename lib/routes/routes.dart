2eec858703f0c3321260a4b96f12d3f6dba860d1
nx006 <limazero14@gmail.com>
2024-07-28 01:12:17 +0900

    feat: group 페이지 라우팅 추가

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 import 'package:hajithon_teami_flutter_app/core/middleware/login.dart';
+import 'package:hajithon_teami_flutter_app/pages/group/group_create/group_create_name_screen.dart';
+import 'package:hajithon_teami_flutter_app/pages/group/group_create/group_create_time_screen.dart';
 import 'package:hajithon_teami_flutter_app/pages/home/home_screen.dart';
 import 'package:hajithon_teami_flutter_app/pages/login/landing_screen.dart';
 import 'package:hajithon_teami_flutter_app/pages/login/login_screen.dart';
@@ -8,10 +10,34 @@ import 'package:hajithon_teami_flutter_app/pages/test/page.dart';
 
 class AppPages {
   static final pages = [
-    GetPage(name: TestScreen.routeName, page: () => const TestScreen()),
-    GetPage(name: LandingScreen.routeName, page: () => const LandingScreen()),
-    GetPage(name: LoginScreen.routeName, page: () => const LoginScreen()),
-    GetPage(name: RegisterScreen.routeName, page: () => const RegisterScreen()),
-    GetPage(name: HomeScreen.routeName, page: () => const HomeScreen(), middlewares: [LoginMiddleware()]),
+    GetPage(
+      name: TestScreen.routeName,
+      page: () => const TestScreen(),
+    ),
+    GetPage(
+      name: LandingScreen.routeName,
+      page: () => const LandingScreen(),
+    ),
+    GetPage(
+      name: LoginScreen.routeName,
+      page: () => const LoginScreen(),
+    ),
+    GetPage(
+      name: RegisterScreen.routeName,
+      page: () => const RegisterScreen(),
+    ),
+    GetPage(
+      name: HomeScreen.routeName,
+      page: () => const HomeScreen(),
+      // middlewares: [LoginMiddleware()],
+    ),
+    GetPage(
+      name: GroupCreateNameScreen.routeName,
+      page: () => const GroupCreateNameScreen(),
+    ),
+    GetPage(
+      name: GroupCreateTimeScreen.routeName,
+      page: () => const GroupCreateTimeScreen(),
+    ),
   ];
 }
