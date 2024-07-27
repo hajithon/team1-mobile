683872746cef3bcef7573e7df34e37e16298a8d0
nx006 <limazero14@gmail.com>
2024-07-28 03:16:38 +0900

    feat: color 추가

--- lib/const/color/color.dart
+++ lib/const/color/color.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 const titleTextColor = Color(0xFF202020);
 
 const subtitleTextColor = Color(0xFF85827F);
@@ -11,3 +9,11 @@ const inputHintColor = Color(0xFFC0BDBA);
 const inputBackgroundColor = Color(0xFFF7F7F7);
 
 const primaryColor = Color(0xFFFF6C3E);
+
+/// 기상
+const orangeBright = Color(0xFFFFB29A);
+
+/// 늦잠
+const brownLight = Color(0xFF94837D);
+
+const brownDark = Color(0xFF5D514D);
